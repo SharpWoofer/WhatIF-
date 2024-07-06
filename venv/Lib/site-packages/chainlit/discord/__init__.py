c7f87c2defa6adc8d9caa6743cf6206921f9fcf1
SharpWoofer <cai_hongqi@yahoo.com>
2024-07-06 16:28:58 +0000

    Delete venv directory

--- venv/Lib/site-packages/chainlit/discord/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    import discord
-except ModuleNotFoundError:
-    raise ValueError(
-        "The discord package is required to integrate Chainlit with a Slack app. Run `pip install discord --upgrade`"
-    )
--- venv/Lib/site-packages/chainlit/teams/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    import botbuilder
-except ModuleNotFoundError:
-    raise ValueError(
-        "The botbuilder-core package is required to integrate Chainlit with a Slack app. Run `pip install botbuilder-core --upgrade`"
-    )
